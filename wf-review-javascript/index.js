56357da0f36aa8978b22b1c67e3bfe42c4072e5c
Fakhruddin Arrozi <fakhruddinarrozi21@gmail.com>
2023-02-27 11:33:45 +0700

    review javascript

---
+++ wf-review-javascript/index.js
@@ -0,0 +1,29 @@
+
+// make array
+const array = ["jhon","aliando",50,1.72,"alex"];
+
+// make array without data
+const arraydua =[];
+
+// add data
+
+arraydua[0] = 12;
+arraydua[1] = 123;
+arraydua[2] = 21;
+arraydua[4] = 12;
+arraydua[5] = "joni";
+
+// push array 
+arraydua.push(1000);
+
+
+const notDuplicateData = new Set();
+
+notDuplicateData.add(12);
+notDuplicateData.add(50);
+notDuplicateData.add(50);
+
+console.log(arraydua);
+
+
+
