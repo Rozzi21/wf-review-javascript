d284be12ca1ca70e2a95fe6f0257bdf3d9e29a8b
Fakhruddin Arrozi <fakhruddinarrozi21@gmail.com>
2023-04-03 09:45:23 +0700

    eventHandler

---
+++ wf-challenge/src/App.js
@@ -0,0 +1,15 @@
+
+import './App.css';
+import React from 'react';
+import ToDo from './ui/ToDo';
+
+const App = () => {
+  return (
+    <div className='App'>
+      <ToDo/>
+    </div>
+    
+  );
+};
+
+export default App;
--- wf-review-javascript/index.js
+++ wf-review-javascript/index.js
@@ -16,7 +16,7 @@ arraydua[5] = "joni";
 // push array 
 arraydua.push(1000);
 
-
+// make set data like array but can't insert duplicate data
 const notDuplicateData = new Set();
 
 notDuplicateData.add(12);
@@ -24,6 +24,29 @@ notDuplicateData.add(50);
 notDuplicateData.add(50);
 
 console.log(arraydua);
+
+// make map like array but using key value
+
+const myMap = new Map([['jatim','surabaya'],['jateng','jogjakarta'],['jabar','bogor']])
+
+// make empty map
+
+let myMyMap = new Map();
+
+// add data to empty map
+
+myMyMap.set('my','mine');
+myMyMap.set('mymy','minemine');
+
+
+// acess map data example
+
+console.log(myMap.get('jatim'));
+console.log(myMap.get('bogor'));
+
+console.log(myMyMap);
+console.log(myMyMap.get('my'));
+
 
 
 
